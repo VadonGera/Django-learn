0013014a49d386591f4138f087e81a59ef9cd385
Vadim Gera <horsespb@yandex.ru>
2024-08-02 16:27:07 +0300

    Сериализатор `CommentSerializer`

--- myproject/todolist/serializers.py
+++ myproject/todolist/serializers.py
@@ -1,10 +1,19 @@
 from rest_framework import serializers
 
-from todolist.models import Task
+from todolist.models import Task, Comment
 
 
+# Сериализатор для задач Task
 class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
+        fields = '__all__'
+
+
+# Сериализатор для комментариев Comment
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
         fields = '__all__'
