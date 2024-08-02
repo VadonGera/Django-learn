ecca87ccb66a41b3d438f5d0d72b0a1b9ef37bae
Vadim Gera <horsespb@yandex.ru>
2024-08-02 17:16:37 +0300

    Изменены сериализаторы `TaskSerializer` и `CommentSerializer`

--- myproject/todolist/serializers.py
+++ myproject/todolist/serializers.py
@@ -8,7 +8,8 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = '__all__'
+        # fields = '__all__'
+        exclude = ['owner']
 
 
 # Сериализатор для комментариев Comment
@@ -16,4 +17,10 @@ class CommentSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Comment
-        fields = '__all__'
+        # fields = '__all__'
+        fields = ['task', 'comment']
+
+    def validate_comment(self, value):
+        if 'bad' in value:
+            raise serializers.ValidationError("недопустимое слово 'bad'")
+        return value
