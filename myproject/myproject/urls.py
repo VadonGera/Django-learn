892dfab848224d18ae7fb9e5135ae3d161b5acd7
Vadim Gera <horsespb@yandex.ru>
2024-08-02 16:49:21 +0300

    CommentListCreateAPIView и CommentRetrieveUpdateDestroyAPIView зарегистрировали в роутере

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todolist.urls', namespace='tasks')),
+    path('api-auth/', include('rest_framework.urls')),
     path('todolist/', include('todolist.urls')),
     path('about/', include('about.urls')),
 ]
--- myproject/todolist/urls.py
+++ myproject/todolist/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
-from .views import base, TodolistViewSet
+from .views import (
+    base,
+    TodolistViewSet,
+    CommentListCreateAPIView,
+    CommentRetrieveUpdateDestroyAPIView,
+)
 from rest_framework.routers import DefaultRouter
 
 
@@ -11,4 +16,6 @@ router.register('tasks', TodolistViewSet)
 
 urlpatterns = [
     path('', base, name='base'),
+    path('comments/', CommentListCreateAPIView.as_view()),
+    path('comments/<int:pk>/', CommentRetrieveUpdateDestroyAPIView.as_view()),
 ] + router.urls
