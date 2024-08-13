2b5e81f9129e7b385248ba47294be4a1219a2f41
Vadim Gera <horsespb@yandex.ru>
2024-08-13 20:55:04 +0300

    Урлы регистрации, авторизации и рефреш токена перенес в приложение users.

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -21,14 +21,14 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-    TokenVerifyView,
-)
+# from rest_framework_simplejwt.views import (
+#     TokenObtainPairView,
+#     TokenRefreshView,
+#     TokenVerifyView,
+# )
 
 
-from users.views import RegisterView, MyTokenObtainPairView
+# from users.views import RegisterView, MyTokenObtainPairView
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -56,9 +56,10 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
-    path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('api/token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # # path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    # path('api/register/', RegisterView.as_view(), name='register'),
 
-    path('api/register/', RegisterView.as_view(), name='register'),
+    path('api/', include('users.urls', namespace='users')),
 ]
--- myproject/users/urls.py
+++ myproject/users/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView
+from users.views import RegisterView, MyTokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 app_name = UsersConfig.name
 
+urlpatterns = [
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    # path('token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('register/', RegisterView.as_view(), name='register'),
+]
